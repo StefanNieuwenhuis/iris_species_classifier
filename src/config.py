bba7e9d7aec6409c845a94bcfe9c2dc2d9604307
Stefan Nieuwenhuis <stefannhs@gmail.com>
2025-05-10 20:54:28 +0200

    add config to store and expose CONSTANTS

---
+++ src/config.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Load variables from .env into environment
+load_dotenv()
+
+# Base directory
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Env-based config
+RAW_DATA_PATH = BASE_DIR / os.getenv("IRIS_DB_PATH")
+IRIS_TABLE_NAME = os.getenv("IRIS_TABLE_NAME", "Iris")
+SEED = int(os.getenv("SEED", 42))
+
+# Derived paths
+DATA_DIR = BASE_DIR / "data"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
