eb02488dea68af146cf7fc504ed7f5dd5a6f4434
Stefan Nieuwenhuis <stefannhs@gmail.com>
2025-05-10 21:03:44 +0200

    update download data script to include .env data and add checks

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
-
 set -e
 
-echo "Downloading Iris dataset via Kaggle CLI..."
+# Load environment variables from .env
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+else
+  echo ".env file not found!"
+  exit 1
+fi
+
+# Check if the database already exists
+if [ -f "$IRIS_DB_PATH" ]; then
+    echo "Database already exists at $IRIS_DB_PATH — skipping download."
+    exit 0
+fi
+
+echo "Downloading Iris dataset to $IRIS_DATA_DIR"
 
 mkdir -p data/raw
 
 # WARNING! This script assumes the presence of Kaggle CLI
-kaggle datasets download -d uciml/iris -p data/raw/iris_species/
+kaggle datasets download -d uciml/iris -p "$IRIS_DATA_DIR"
 echo "✅ Download complete."
 
-cd data/raw/iris_species && unzip -o iris.zip && rm iris.zip
+cd "$IRIS_DATA_DIR" && unzip -o iris.zip && rm iris.zip
 echo "✅ Unzip complete."
