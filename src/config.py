1140e94a789b6ebd98cbcf6764601407e3db6ea2
Stefan Nieuwenhuis <stefannhs@gmail.com>
2025-05-14 14:51:29 +0200

    add comment to lazy-loading iris db path

--- src/config.py
+++ src/config.py
@@ -6,10 +6,15 @@ from dotenv import load_dotenv
 env_file = Path(".env") if Path(".env").exists() else Path(".env.ci")
 load_dotenv(dotenv_path=env_file)
 
+# Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 def get_raw_data_path() -> Path:
+    """
+    Lazy-load IRIS_DB_PATH to compute its value at runtime.
+    This allows patched environment variables to take effect.
+    """
     env_path = os.getenv("IRIS_DB_PATH")
     if env_path is None:
         raise RuntimeError("Environment variable IRIS_DB_PATH is not set.")
